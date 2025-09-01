9dd8ad85cff6b2624d3cdf3ca6b76e9654028be4
Kimitri <kimitrimusic@gmail.com>
2025-09-01 20:38:14 -0300

    feat: Make FX move more stable, add case-insensitive search and pending move queue

--- Scripts/VU_FX_Keeper/VU_FX_Keeper.lua
+++ Scripts/VU_FX_Keeper/VU_FX_Keeper.lua
@@ -1,7 +1,6 @@
 -- @description Move new FX before the chosen FX only when there is a change
--- @version 1.0
+-- @version 1.2
 -- @author Kimitri
-
 
 local EXT_SECTION = "MoveFXBefore"
 local EXT_KEY = "vu_name"
@@ -9,7 +8,6 @@ local EXT_KEY = "vu_name"
 -- try to load saved value
 local vu_name = reaper.GetExtState(EXT_SECTION, EXT_KEY)
 
--- if it doesn't exist, ask the user
 if vu_name == "" then
     local ret, user_input = reaper.GetUserInputs("Select target FX", 1, "FX name (or part of it):", "")
     if not ret or user_input == "" then
@@ -23,6 +21,18 @@ end
 local last_fx_count = {}
 local vu_index_cache = {}
 local last_state = reaper.GetProjectStateChangeCount(0)
+local pending_moves = {}
+
+-- find target FX
+local function findVU(track, fx_count)
+    for j = 0, fx_count - 1 do
+        local _, fx_name = reaper.TrackFX_GetFXName(track, j, "")
+        if fx_name:lower():find(vu_name:lower(), 1, true) then
+            return j
+        end
+    end
+    return nil
+end
 
 local function checkFX()
     local state = reaper.GetProjectStateChangeCount(0)
@@ -36,33 +46,35 @@ local function checkFX()
             local prev_count = last_fx_count[i] or 0
 
             if fx_count > prev_count then
-                -- find or use cached VU
-                local vu_index = vu_index_cache[i]
-                if not vu_index or vu_index >= fx_count then
-                    vu_index = nil
-                    for j = 0, fx_count - 1 do
-                        local _, fx_name = reaper.TrackFX_GetFXName(track, j, "")
-                        if fx_name:find(vu_name, 1, true) then
-                            vu_index = j
-                            break
-                        end
-                    end
-                    vu_index_cache[i] = vu_index
-                end
-
-                if vu_index then
-                    for fx = prev_count, fx_count - 1 do
-                        if fx > vu_index then
-                            reaper.TrackFX_CopyToTrack(track, fx, track, vu_index, true)
-                            vu_index = vu_index + 1
-                        end
-                    end
-                    vu_index_cache[i] = vu_index
-                end
+                -- schedule repositioning for the next cycle
+                pending_moves[i] = {prev_count = prev_count, fx_count = fx_count}
             end
 
             last_fx_count[i] = fx_count
         end
+    end
+
+    -- process pending moves (one cycle after detection)
+    for i, data in pairs(pending_moves) do
+        local track = reaper.GetTrack(0, i)
+        local vu_index = vu_index_cache[i]
+
+        if not vu_index or vu_index >= data.fx_count then
+            vu_index = findVU(track, data.fx_count)
+            vu_index_cache[i] = vu_index
+        end
+
+        if vu_index then
+            for fx = data.prev_count, data.fx_count - 1 do
+                if fx > vu_index then
+                    reaper.TrackFX_CopyToTrack(track, fx, track, vu_index, true)
+                    vu_index = vu_index + 1
+                end
+            end
+            vu_index_cache[i] = vu_index
+        end
+
+        pending_moves[i] = nil -- clear the pending move for this track
     end
 
     reaper.defer(checkFX)
